dc7a2a306678dcbbcc547cb86010798472f49267
ozodbekabdullajonov12 <ozodbekabdullajonov1@gmail.com>
2024-12-19 11:47:36 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MainCategory());
+}
+
+class MainCategory extends StatelessWidget {
+  const MainCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
+
